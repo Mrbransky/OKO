4cd9309ec1a92139575e86961b2b0f303c7ef78e
zoranac <michael@pattengale.org>
2015-03-31 05:00:54 -0500

    Planet Spawn Semi-Equidistant from each other
    Next to add planets number dependent on number of players and to scale
    the OoB and min and max radius with the number of planets. Also Need to
    set up player spawning around the planets.

--- Assets/MainScripts/PlanetSpawner.cs
+++ Assets/MainScripts/PlanetSpawner.cs
@@ -6,9 +6,12 @@ public class PlanetSpawner : MonoBehaviour {
 
 	public List<Sprite> PlanetSprites;
 	public GameObject PlanetPrefab;
-
+	
 	[Range(1,4)]
 	public int NumberOfPlanets = 1;
+
+	public float MinRadius;
+	public float MaxRadius;
 
 	// Use this for initialization
 	void Start () {
@@ -19,8 +22,15 @@ public class PlanetSpawner : MonoBehaviour {
 			spawnedPlanet.GetComponent<SpriteRenderer> ().sprite = PlanetSprites [Random.Range (0, PlanetSprites.Count)];
 		}
 		else{
+			float angle = Random.Range(0,Mathf.PI * 2);
 			for (int i = 0; i < NumberOfPlanets; i++) {
-						Vector2 position = new Vector2 (Random.Range (-100, 100), Random.Range (-100, 100));
+				float radius = Random.Range(/*Min Radius*/MinRadius, /*MaxRadius*/MaxRadius);
+				angle += Random.Range(((Mathf.PI * 2/NumberOfPlanets)-(Mathf.PI / 3/NumberOfPlanets)), ((Mathf.PI * 2/NumberOfPlanets)+(Mathf.PI / 3/NumberOfPlanets)));
+
+				print (radius + " " + Mathf.Rad2Deg * angle);
+
+				Vector2 position = (new Vector2 (Mathf.Cos(angle), 
+				                                 Mathf.Sin(angle))) * radius;
 						GameObject spawnedPlanet = (GameObject)Instantiate (PlanetPrefab, position, Quaternion.identity);
 						spawnedPlanet.GetComponent<SpriteRenderer> ().sprite = PlanetSprites [Random.Range (0, PlanetSprites.Count)];
 			}
