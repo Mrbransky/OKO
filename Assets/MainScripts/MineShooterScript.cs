60989302cccc916b2a9ec2313f56b8b313a42790
zoranac <michael@pattengale.org>
2015-06-18 19:35:55 -0500

    Added charging time for mine shooters
    so now you can't just spam mines

--- Assets/MainScripts/MineShooterScript.cs
+++ Assets/MainScripts/MineShooterScript.cs
@@ -2,35 +2,30 @@
 using System.Collections;
 
 public class MineShooterScript : MonoBehaviour {
-	public float CircleSpeed;
-	public float MaxForwardSpeed = 1; // Assuming negative Z is towards the camera
-	public float MinForwardSpeed = 1;
-	float forwardSpeed;
-	float counter = 0;
-	public float circleSize = 8;
-	public KeyCode MyKey;
-	float startingPos;
-	float lastAngle = 0;
-	float angle = 0;
+	public float CircleSpeed;				//The speed at which the shooter circles the arena
+	public float circleSize = 8;			//the size of the arena of which to circle around
+	public KeyCode MyKey;					//the key that controls the shooter
+	float startingPos;						//the starting position of the shooter randomly placed in a circle around the arena
+	float angle = 0;						//the angle of the shooter in relation to the center of the arena
 
-	float buttonPressedLastTime = 0;
-	float buttonPressedDuration = 0;
-	float lastButtonPressedDuration = 100;
-	float durationBetweenPresses = 0;
-	float shotWaitTime = 0;
-	public float ShotRechargeTime = 0;
-	public GameObject Projectile;
-	public float DefaultShotForce;
-	public float MaxShotForce;
-	float shotForce;
-	public float ChargeAmount;
-	GameObject camera;
-	private Vector3 curLoc;
-	private Vector3 prevLoc;
-	bool shot = false;
-	bool inPosition = false;
-	bool stop = false;
-	bool doubletapped = false;
+	float buttonPressedLastTime = 0;		//Time the button was pressed last
+	float buttonPressedDuration = 0;		//the duration the button was held down for
+	float lastButtonPressedDuration = 100;	//the duration the button was held down for Last
+	float durationBetweenPresses = 0;		//the duration the between button presses
+	float shotWaitTime = 0;					//Sets to the currect time when the button is released to add a short tiem period to see if it will be a double tap or not
+	public float ShotRechargeDuration = 0;	//The amount of time it takes before you can fire a new shot
+	float shotRechargeTime = 0;				//The time at which you fire a shot to restart the recharge
+	public GameObject Projectile;			//the object you shoot
+	public float DefaultShotForce;			//the base force you shoot at. should be a small number
+	public float MaxShotForce;				//the most force you can shoot at.
+	float shotForce;						//the current shot force, determined by how long you hold down the button
+	public float ChargeAmount; 				//the amound the shotforce is increased by every frame the button is held down	
+	GameObject camera;						//the camera	
+	bool shot = false;						//if true, the button press is a shot, not a double tap.
+	bool inPosition = false;				//if true, the shooter is locked in orbit around the arena
+	bool stop = false;						//if true, stops the shooter from moving towards the center of the arena and prepares to lock it into place
+	bool doubletapped = false;				//if true, the button press is a double tap, not a shot
+	bool recharged = false;					//if true, the shooter is recharged and can be fired again
 
 	// Use this for initialization
 	void Start () {
@@ -57,25 +52,39 @@ public class MineShooterScript : MonoBehaviour {
 			if (Time.time >= shotWaitTime + .26f && shot)
 			{
 				//shoot mine
-				print("Shoot");
-				GameObject tempMine = (GameObject)Instantiate(Projectile,transform.position,transform.rotation);
-				tempMine.GetComponent<Rigidbody2D>().velocity = -transform.position.normalized * shotForce;
-				shot = false;
-				shotForce=DefaultShotForce;
+				if (recharged)
+				{
+					print("Shoot");
+					GameObject tempMine = (GameObject)Instantiate(Projectile,transform.position,transform.rotation);
+					tempMine.GetComponent<Rigidbody2D>().velocity = -transform.position.normalized * shotForce;
+					shot = false;
+					shotForce=DefaultShotForce;
+					shotRechargeTime = Time.time;
+				}
 			}
-
+			if (Time.time <= shotRechargeTime + ShotRechargeDuration)
+			{
+				recharged = false;
+			}
+			else{
+				recharged = true;
+			}
 			if(Input.GetKeyDown(MyKey))
 			{
 
 				durationBetweenPresses = Time.time - buttonPressedLastTime;
 				buttonPressedLastTime = Time.time;
 				buttonPressedDuration = Time.time - buttonPressedLastTime;
+				bool reset = false; //To fix the issue of triple+ tapping functioning like a double double tap
+				if (doubletapped)
+				{
+					doubletapped = false;
+					reset = true;
+				}
 
-				if (lastButtonPressedDuration <= .25f && durationBetweenPresses <= .25f)
+				if (lastButtonPressedDuration <= .25f && durationBetweenPresses <= .25f && !reset)
 				{
 					shot = false;
-					//change direction	
-					//counter = angle;
 					CircleSpeed = -CircleSpeed;
 					lastButtonPressedDuration = 100;
 					buttonPressedDuration = 0;
@@ -86,6 +95,10 @@ public class MineShooterScript : MonoBehaviour {
 					lastButtonPressedDuration = 100;
 					buttonPressedDuration = 0;
 					doubletapped = false;
+					if (!recharged)
+					{
+						//play not recharged sound
+					}
 
 				}
 
@@ -93,10 +106,13 @@ public class MineShooterScript : MonoBehaviour {
 			if(Input.GetKey(MyKey))
 			{
 				buttonPressedDuration = Time.time - buttonPressedLastTime;
-				if (shotForce < MaxShotForce)
-					shotForce += ChargeAmount;
-				else
-					shotForce = MaxShotForce;
+				if (recharged)
+				{
+					if (shotForce < MaxShotForce)
+						shotForce += ChargeAmount;
+					else
+						shotForce = MaxShotForce;
+				}
 			}
 			if (Input.GetKeyUp(MyKey))
 			{
